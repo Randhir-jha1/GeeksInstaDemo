d895232597db1dad0d9359940f7e2b066c12e57f
Actimobiledev <actiknow.mobiledev@gmail.com>
2023-07-30 00:13:28 +0530

    code_push_after_first_class

---
+++ app/src/main/java/com/rj/geeksinstademo/fragments/NotifiactionFragment.kt
@@ -0,0 +1,19 @@
+package com.rj.geeksinstademo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rj.geeksinstademo.R
+
+class NotifiactionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_notification, container, false)
+    }
+
+}
