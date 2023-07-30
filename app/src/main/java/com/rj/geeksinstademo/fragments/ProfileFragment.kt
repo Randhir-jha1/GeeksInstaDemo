c35455036bd1c7fbbba681494fa13ab50904f76a
Actimobiledev <actiknow.mobiledev@gmail.com>
2023-07-30 22:14:03 +0530

    code_push_after_second_class

--- app/src/main/java/com/rj/geeksinstademo/fragments/ProfileFragment.kt
+++ app/src/main/java/com/rj/geeksinstademo/fragments/ProfileFragment.kt
@@ -1,19 +1,32 @@
 package com.rj.geeksinstademo.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.rj.geeksinstademo.OthersActivity
 import com.rj.geeksinstademo.R
+import com.rj.geeksinstademo.databinding.FragmentHomeBinding
+import com.rj.geeksinstademo.databinding.FragmentProfileBinding
+import com.rj.geeksinstademo.databinding.FragmentSearchBinding
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater,container,false)
+        val view = binding.root
+        binding.options.setOnClickListener(){
+            startActivity(Intent(requireActivity(),OthersActivity::class.java))
+        }
+        return view
+
     }
 
 }
---
+++ app/src/main/java/com/rj/geeksinstademo/model/Notification.kt
@@ -0,0 +1,3 @@
+package com.rj.geeksinstademo.model
+
+data class Notification(val userid: String? = "",val text: String? = "",val postid: String? = "",val isPost: Boolean?=false)
